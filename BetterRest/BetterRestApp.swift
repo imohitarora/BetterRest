ddb491b1a0804fcb5a2fb9d19e2861d7fd77e839
Mohit Arora <me@aboutmohit.com>
2024-04-23 14:25:01 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mohit Arora on 2024-04-23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
